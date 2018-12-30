5d25703eaa2f2b7f33ef44cc349949c45f7e29cb
stephengtuggy <sgt@stephengtuggy.com>
2018-12-29 19:35:39 -0800

    Switch database engine to Postgres

--- jobHistorySite/jobHistorySite/settings.py
+++ jobHistorySite/jobHistorySite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'jobHistorySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobHistoryDB',
+        'USER': 'stephengtuggy',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
