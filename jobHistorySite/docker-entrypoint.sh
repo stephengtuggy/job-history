d5570e3c31d24556eeb20e62fe8cfbd7ae05e329
stephengtuggy <sgt@stephengtuggy.com>
2019-08-03 01:56:08 -0700

    fix(docker-entrypoint.sh): Uncomment, fix up check for existence of backup_to_restore.sql

--- jobHistorySite/docker-entrypoint.sh
+++ jobHistorySite/docker-entrypoint.sh
@@ -2,10 +2,11 @@
 
 ./tcp-port-wait.sh $JOB_HISTORY_DB_HOST $JOB_HISTORY_DB_PORT
 
-# if [ -f "./backup_to_restore.sql" ]
-#     echo "backup_to_restore.sql exists"
-#     #PGPASSWORD=$JOB_HISTORY_DB_PASSWORD psql -U $JOB_HISTORY_DB_USERNAME -d $JOB_HISTORY_DB_NAME -h $JOB_HISTORY_DB_HOST -p $JOB_HISTORY_DB_PORT < backup_to_restore.sql
-# fi
+if [ -f "./backup_to_restore.sql" ]
+then
+    echo "backup_to_restore.sql exists; restoring it"
+    PGPASSWORD=$JOB_HISTORY_DB_PASSWORD psql -U $JOB_HISTORY_DB_USERNAME -d $JOB_HISTORY_DB_NAME -h $JOB_HISTORY_DB_HOST -p $JOB_HISTORY_DB_PORT < ./backup_to_restore.sql
+fi
 ./manage.py migrate --no-input
 
 mkdir -p $JOB_HISTORY_STATIC_ROOT
