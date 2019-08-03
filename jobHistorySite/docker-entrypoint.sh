ca1b46b1f075d1c06f5ed88fec2a662e24cbe4eb
stephengtuggy <sgt@stephengtuggy.com>
2019-08-02 17:27:39 -0700

    fix(docker-compose.yml,docker-entrypoint.sh): web finds db successfully now

--- jobHistorySite/docker-entrypoint.sh
+++ jobHistorySite/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-./tcp-port-wait.sh 0.0.0.0 $JOB_HISTORY_DB_PORT
+./tcp-port-wait.sh $JOB_HISTORY_DB_HOST $JOB_HISTORY_DB_PORT
 
 # if [ -f "backup_to_restore.sql" ]
 
 # fi
-./manage.py migrate
+./manage.py migrate --no-input
 
 mkdir -p $JOB_HISTORY_STATIC_ROOT
 # ???
-./manage.py collectstatic
+./manage.py collectstatic --no-input
 
 # Replace eventually with execution of $@ ?
 ./manage.py runserver 0.0.0.0:8000
