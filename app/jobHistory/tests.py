1b78cb9361ddc75c2fe94bdcdc40f1d0707f176a
Stephen G Tuggy <sgt@stephengtuggy.com>
2020-08-21 14:46:36 -0700

    feat(tests): First test

--- app/jobHistory/tests.py
+++ app/jobHistory/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Employer, Position, JobTimePeriod
 
 # Create your tests here.
+class EmployerTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_creating_an_employer(self):
+        Employer.objects.create(short_name="Employer1", long_name="Employer 1")
+
+    # def test_creating_positions(self):
+    #     Employer.objects.create(short_name="Employer2", long_name="Employer 2")
+    #     empID = Employer.objects.get(short_name="Employer2").pk
+    #     Position.objects.create(employer=empID, title="Position 1", supervisor_given_name="John", supervisor_surname="Doe", can_contact=True)
+    #     Position.objects.create(employer=empID, title="Position 2", supervisor_given_name="Jane", supervisor_surname="Doe", can_contact=False)
+
+    # def test_creating_job_time_periods(self):
+    #     Employer.objects.create(short_name="Employer3", long_name="Employer 3")
+    #     empID = Employer.objects.get(short_name="Employer3").pk
+    #     Position.objects.create(employer=empID, title="Position 3", supervisor_given_name="John", supervisor_surname="Doe", can_contact=True)
+    #     Position.objects.create(employer=empID, title="Position 4", supervisor_given_name="Jane", supervisor_surname="Doe", can_contact=False)
+    #     position3ID = Position.objects.get()
+
+
